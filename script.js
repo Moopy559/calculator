8da4cb70d3db18d7449ae5efd808328ee34eef83
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2023-08-27 17:43:45 +1000

    add basic operator functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+console.log(add(2, 3));
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+console.log(subtract(2, 3));
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+console.log(multiply(2, 3));
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+console.log(divide(2, 3));
