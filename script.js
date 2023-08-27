484a24b2008989d9b2e89a4c3d8c1c70ab052746
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2023-08-28 08:10:43 +1000

    add Operate() function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Basic operator functions
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -17,3 +18,14 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 console.log(divide(2, 3));
+
+// Variables for operator functions
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+
+function operate(firstNumber, secondNumber) {
+    return add(firstNumber, secondNumber);
+}
+
+console.log(operate(1,2));
