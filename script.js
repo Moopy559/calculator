228dca7f1a25d6a8986f9c41e1401427d0b65a81
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2023-08-28 10:02:14 +1000

    add Number input functionality

--- script.js
+++ script.js
@@ -2,30 +2,34 @@
 function add(num1, num2) {
   return num1 + num2;
 }
-console.log(add(2, 3));
 
 function subtract(num1, num2) {
   return num1 - num2;
 }
-console.log(subtract(2, 3));
 
 function multiply(num1, num2) {
   return num1 * num2;
 }
-console.log(multiply(2, 3));
 
 function divide(num1, num2) {
   return num1 / num2;
 }
-console.log(divide(2, 3));
 
 // Variables for operator functions
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber = '';
+let secondNumber = '';
 let operator = '';
 
 function operate(firstNumber, secondNumber) {
     return add(firstNumber, secondNumber);
 }
 
-console.log(operate(1,2));
+let isNumber = document.querySelectorAll('.numbers');
+console.log(isNumber);
+
+isNumber.forEach(function(button) {
+  button.addEventListener('click', function() {
+    firstNumber += button.textContent;
+    console.log(firstNumber);
+  });
+});
